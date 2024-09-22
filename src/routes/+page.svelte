49d35d9f14a5d0adb2a25e69edb4bd13f7557864
Abdullah Basheer <digitaldrreamer@gmail.com>
2024-09-22 19:22:08 +0100

    feat: Revamp UI with Bits UI and typography enhancements
    Integrated Bits UI for enhanced UI components and improved overall styling. Introduced custom font styles for a cohesive and visually appealing design.  Replaced Flowbite-Svelte with Bits UI for a more robust and feature-rich component library.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 		<div class="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-16">
 			<div class="mx-auto max-w-2xl text-center">
 				<p
-					class="inline-block bg-gradient-to-l from-primary-600 to-violet-500 bg-clip-text text-sm font-medium text-transparent dark:from-primary-400 dark:to-violet-400"
+					class="font-secondary text-md inline-block bg-gradient-to-l from-primary-600 to-violet-500 bg-clip-text font-medium text-transparent dark:from-primary-400 dark:to-violet-400"
 				>
 					Snippeteer: Code Sharing Reimagined
 				</p>
@@ -106,7 +106,7 @@
 	<div class="mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8">
 		<div class="mx-auto max-w-2xl text-center">
 			<h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl dark:text-white">
-				Let the world see your <span class="text-primary-600">code</span>
+				Let the world see your <span class="font-secondary text-primary-600">code</span>
 			</h1>
 			<p class="mt-3 text-lg text-gray-800 dark:text-neutral-400">
 				Write the code you love. Even if it's a simple Hello World.
@@ -122,21 +122,21 @@
 				<div class="flex size-full flex-col items-center justify-center">
 					<a
 						class="inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-3 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 focus:bg-gray-50 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-						href="#"
+						href="https://github.com/digitaldrreamer/snippeteer/"
 					>
-						<svg
-							class="size-4 shrink-0"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							viewBox="0 0 24 24"
-							fill="none"
-							stroke="currentColor"
-							stroke-width="2"
-							stroke-linecap="round"
-							stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3" /></svg
-						>
-						See us on Github
+						<svg class="size-4 shrink-0" viewBox="0 0 128 128">
+							<g fill="#181616"
+								><path
+									fill-rule="evenodd"
+									clip-rule="evenodd"
+									d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"
+								></path><path
+									d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"
+								></path></g
+							>
+						</svg>
+
+						Star us on Github
 					</a>
 				</div>
 			</div>
--- src/routes/snippets/new/+page.svelte
+++ src/routes/snippets/new/+page.svelte
@@ -1,51 +1,63 @@
 <script>
-	import { Tabs, TabItem } from 'flowbite-svelte';
+	import * as Tabs from '$lib/components/ui/tabs';
 </script>
 
-<Tabs tabStyle="underline">
-	<TabItem open>
-		<div slot="title" class="flex items-center gap-2">
-			<img
-				src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
-				alt="html logo"
-			/>
-			HTML5
-		</div>
-		<p class="text-sm text-gray-500 dark:text-gray-400">
-			<b>Profile:</b>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-			et dolore magna aliqua.
-		</p>
-	</TabItem>
-	<TabItem>
-		<div slot="title" class="flex items-center gap-2">
-			<img
-				src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
-				alt="css3 logo"
-			/>
-			CSS3
-		</div>
-		<p class="text-sm text-gray-500 dark:text-gray-400">
-			<b>Dashboard:</b>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-			et dolore magna aliqua.
-		</p>
-	</TabItem>
-	<TabItem>
-		<div slot="title" class="flex items-center gap-2">
-			<img
-				src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-				alt="javscript"
-			/>
-			JS
-		</div>
-		<p class="text-sm text-gray-500 dark:text-gray-400">
-			<b>Settings:</b>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-			et dolore magna aliqua.
-		</p>
-	</TabItem>
-</Tabs>
+<div
+	class="block h-[100svh] space-y-4 px-4 py-4 leading-10 sm:grid sm:grid-flow-col sm:grid-rows-3 sm:gap-4 sm:space-y-0"
+>
+	<div
+		class="block h-[60vh] w-full rounded-xl bg-neutral-100 p-4 shadow-md shadow-neutral-300 sm:col-span-3 sm:row-span-3 sm:h-auto dark:bg-neutral-800 dark:shadow-neutral-900"
+	>
+		<Tabs.Root value="html" class="h-full w-full border border-dashed">
+			<Tabs.List
+				class="grid w-full grid-cols-3 space-x-3 bg-slate-200 text-neutral-900 dark:bg-slate-900"
+			>
+				<Tabs.Trigger class="space-x-1 align-middle" value="html">
+					<span> HTML5 </span>
+					<img
+						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+						alt="html logo"
+						class="size-4"
+					/>
+				</Tabs.Trigger>
+				<Tabs.Trigger class="space-x-1 align-middle" value="css">
+					<span> CSS3 </span>
+					<img
+						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+						alt="css3 logo"
+						class="size-4"
+					/>
+				</Tabs.Trigger>
+				<Tabs.Trigger class="space-x-1 align-middle" value="js">
+					<img
+						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+						alt="javscript logo"
+						class="size-4"
+					/>
+					<span> JS/TS </span>
+					<img
+						src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+						alt="typescript logo"
+						class="size-4"
+					/>
+				</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="html">Make changes to your account here.</Tabs.Content>
+			<Tabs.Content value="css">Change your password here.</Tabs.Content>
+			<Tabs.Content value="js">Make changes to your account here.</Tabs.Content>
+		</Tabs.Root>
+	</div>
+	<div
+		class="block h-[20vh] w-full rounded-xl bg-neutral-100 p-4 shadow-md shadow-neutral-300 sm:col-span-1 sm:h-auto dark:bg-neutral-800 dark:shadow-neutral-900"
+	>
+		&nbsp;
+	</div>
+	<div
+		class="block h-[60vh] w-full rounded-xl bg-neutral-100 p-4 shadow-md shadow-neutral-300 sm:col-span-1 sm:row-span-2 sm:h-auto dark:bg-neutral-800 dark:shadow-neutral-900"
+	>
+		&nbsp;
+	</div>
+</div>
 
 <!-- 
             TS <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
