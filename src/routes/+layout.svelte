ac15afc1e8c61f86a156bc3e70c033ac0edd3bbb
Abdullah Basheer <digitaldrreamer@gmail.com>
2024-09-23 17:57:21 +0100

    feat: Scope editor menu bar to snippet creation page
    The editor menu bar is now only displayed on the snippet creation page. This enhances the user experience by providing relevant tools only when needed and reducing visual clutter on other pages.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,42 +19,44 @@
 <Navbar />
 
 <Toaster richColors />
-<div class=" relative h-full w-full">
+<div class="relative h-full w-full">
 	<slot />
-	<Menubar.Root class="fixed bottom-5 left-1/2 sm:left-5">
-		<Menubar.Menu>
-			<Menubar.Trigger>Edit</Menubar.Trigger>
-			<Menubar.Content>
-				<Menubar.Item>
-					Undo <Menubar.Shortcut>⌘Z</Menubar.Shortcut>
-				</Menubar.Item>
-				<Menubar.Item>
-					Redo <Menubar.Shortcut>⌘Y</Menubar.Shortcut>
-				</Menubar.Item>
-				<Menubar.Item>
-					Save <Menubar.Shortcut>⇧⌘S</Menubar.Shortcut>
-				</Menubar.Item>
-			</Menubar.Content>
-		</Menubar.Menu>
-		<Menubar.Menu>
-			<Menubar.Trigger>Live Preview</Menubar.Trigger>
-			<Menubar.Content>
-				<Menubar.CheckboxItem bind:checked={bookmarks}>Reload</Menubar.CheckboxItem>
-				<Menubar.Item inset>
-					New tab <Menubar.Shortcut>⌘<Kbd>Alt</Kbd>O</Menubar.Shortcut>
-				</Menubar.Item>
-			</Menubar.Content>
-		</Menubar.Menu>
-		<Menubar.Menu>
-			<Menubar.Trigger>Settings</Menubar.Trigger>
-			<Menubar.Content>
-				<Menubar.Separator />
-				<Menubar.Item inset>Profile</Menubar.Item>
-				<Menubar.Separator />
-				<Menubar.Item inset>Saved Snippets</Menubar.Item>
-				<Menubar.Separator />
-				<Menubar.Item inset>Switch Theme</Menubar.Item>
-			</Menubar.Content>
-		</Menubar.Menu>
-	</Menubar.Root>
+	{#if browser && window.location.pathname === '/snippets/new'}
+		<Menubar.Root class="fixed bottom-5 left-1/2 sm:left-5">
+			<Menubar.Menu>
+				<Menubar.Trigger>Edit</Menubar.Trigger>
+				<Menubar.Content>
+					<Menubar.Item>
+						Undo <Menubar.Shortcut>⌘Z</Menubar.Shortcut>
+					</Menubar.Item>
+					<Menubar.Item>
+						Redo <Menubar.Shortcut>⌘Y</Menubar.Shortcut>
+					</Menubar.Item>
+					<Menubar.Item>
+						Save <Menubar.Shortcut>⇧⌘S</Menubar.Shortcut>
+					</Menubar.Item>
+				</Menubar.Content>
+			</Menubar.Menu>
+			<Menubar.Menu>
+				<Menubar.Trigger>Live Preview</Menubar.Trigger>
+				<Menubar.Content>
+					<Menubar.CheckboxItem bind:checked={bookmarks}>Reload</Menubar.CheckboxItem>
+					<Menubar.Item inset>
+						New tab <Menubar.Shortcut>⌘<Kbd>Alt</Kbd>O</Menubar.Shortcut>
+					</Menubar.Item>
+				</Menubar.Content>
+			</Menubar.Menu>
+			<Menubar.Menu>
+				<Menubar.Trigger>Settings</Menubar.Trigger>
+				<Menubar.Content>
+					<Menubar.Separator />
+					<Menubar.Item inset>Profile</Menubar.Item>
+					<Menubar.Separator />
+					<Menubar.Item inset>Saved Snippets</Menubar.Item>
+					<Menubar.Separator />
+					<Menubar.Item inset>Switch Theme</Menubar.Item>
+				</Menubar.Content>
+			</Menubar.Menu>
+		</Menubar.Root>
+	{/if}
 </div>
